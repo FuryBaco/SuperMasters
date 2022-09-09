7b1d5003cd5b539e88ff0486abbef53bf0722664
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-09-09 07:46:53 -0500

    Creacion de las entidades Empresa, Empleado y Movimiento

--- src/main/java/com/SuperMasters/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/SuperMasters/Ciclo3/Ciclo3Application.java
@@ -1,13 +1,28 @@
 package com.SuperMasters.Ciclo3;
 
+import com.SuperMasters.Ciclo3.entities.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Ciclo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		Empresa empresa = new Empresa("CapsuleCorp", "Calle 123", "123456789", "123456789");
+		return empresa.getNombre();
 	}
 
 }
